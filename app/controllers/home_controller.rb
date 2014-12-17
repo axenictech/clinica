f234b1ee0c8ba5532f1ada0b602261b01cd21a53
ravikiran kadam <ravikirankadam.1991@gmail.com>
2014-12-17 11:34:47 +0530

    clinica change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,14 @@ class HomeController < ApplicationController
 
   def patient_checkup_form
     @patient_info = NewPatient.find(params[:format])
-    @doctor = params[:doctor_id]
+    @doctor = DoctorMaster.find(params[:doctor_id])
     @patient = PatientCheckup.new
   end
 
   def save_patient_checkup_information
     @patient_id = params[:patient_id]
     @doctor_id = params[:doctor_id]
-    @patient = PatientCheckup.new(save_patient_checkup_information_params)
+    @patient = PatientCheckup.new(patient_checkup_information_params)
     if @patient.save
       @patient.update(new_patient_id:@patient_id)
       @patient.update(references_doctor_id:@doctor_id)
@@ -24,13 +24,24 @@ class HomeController < ApplicationController
   end
 
   def edit_patient_checkup_form
-    @patient = PatientCheckup.find(params[:format])
-    p @patient
+    @patient = PatientCheckup.find_by_new_patient_id(params[:format])
+    @patient_info = NewPatient.find(params[:format])
+    @doctor = DoctorMaster.find(@patient.references_doctor_id)
+  end
+
+  def update_patient_checkup_information
+    @patient = PatientCheckup.find(params[:id])
+      if @patient.update(patient_checkup_information_params)
+        flash[:notice] = 'Patient checkup information updated successfully!'
+        redirect_to outdoors_todays_patient_path
+      else
+        render 'edit_patient_checkup_form'
+      end
   end
 
   private
 
-  def save_patient_checkup_information_params
+  def patient_checkup_information_params
     params.require(:patient_checkup).permit!
   end
 end
